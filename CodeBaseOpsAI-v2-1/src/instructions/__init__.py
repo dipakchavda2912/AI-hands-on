4e830cc4c7a1860131ecbf8d91c60ba943600681
Dipak Chavda <dipak.chavda@toyota.com>
2026-01-08 14:24:12 +0530

    feat: Remove deprecated serverless instructions files

--- CodeBaseOpsAI-v2-1/src/instructions/__init__.py
+++ CodeBaseOpsAI-v2-1/src/instructions/__init__.py
@@ -3,17 +3,15 @@ Instructions package - Centralized instruction utilities
 """
 
 from .github_instructions import GithubInstructions
-from .serverless_custom_instructions import ServerlessCustomInstructions
-from .serverless_provider_instructions import ServerlessProviderInstructions
-from .serverless_packages_instructions import ServerlessPackagesInstructions
 from .nodejs_packages_instructions import NodeJsPackagesInstructions
+from .serverless_custom_tag_instructions import ServerlessCustomTagInstructions
+from .serverless_providers_tag_instructions import ServerlessProvidersTagInstructions
 from .serverless_plugins_tag_instructions import ServerlessPluginsTagInstructions
 
 __all__ = [
     'GithubInstructions',
-    'ServerlessCustomInstructions',
-    'ServerlessProviderInstructions',
-    'ServerlessPackagesInstructions',
     'NodeJsPackagesInstructions',
+    'ServerlessCustomTagInstructions',
+    'ServerlessProvidersTagInstructions',
     'ServerlessPluginsTagInstructions',
 ]
